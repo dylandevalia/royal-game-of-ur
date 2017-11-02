1adacfeb3b4a2e1be0c05534657c29ea421bbecd
Dylan Devalia <dylan@devalia.com>
2017-11-02 14:33:57 +0000

    Added colours to each of the different levels to make them easier to read

--- utility/src/game/dylandevalia/royal_game_of_ur/utility/Log.java
+++ utility/src/game/dylandevalia/royal_game_of_ur/utility/Log.java
@@ -16,53 +16,63 @@ public class Log {
 	private static int level = LEVEL_ALL;
 	private static File file;
 	
-	public static void ALL() {
+	public static void SET_ALL() {
 		level = LEVEL_ALL;
 	}
-	public static void TRACE() {
+	public static void SET_TRACE() {
 		level = LEVEL_TRACE;
 	}
-	public static void DEBUG() {
+	public static void SET_DEBUG() {
 		level = LEVEL_DEBUG;
 	}
-	public static void INFO() {
+	public static void SET_INFO() {
 		level = LEVEL_INFO;
 	}
-	public static void WARN() {
+	public static void SET_WARN() {
 		level = LEVEL_WARN;
 	}
-	public static void ERROR() {
+	public static void SET_ERROR() {
 		level = LEVEL_ERROR;
 	}
-	public static void NONE() {
+	public static void SET_NONE() {
 		level = LEVEL_NONE;
 	}
 	
+	/**
+	 * Logs a message to file and console
+	 * @param level     The log level (eg. LEVEL_TRACE or LEVEL_INFO)
+	 * @param category  Category of the log
+	 * @param message   The message to log
+	 * @param ex        Throwable exception
+	 */
 	private static void log(int level, String category, String message, Throwable ex) {
+		// Builds final message
 		Date date = new Date();
-		// Build string
 		StringBuilder builder = new StringBuilder(255);
 		builder.append(new SimpleDateFormat("HH:mm:ss").format(date));
 		builder.append(' ');
 		switch (level) {
-			case 5: builder.append("ERROR:"); break;
-			case 4: builder.append(" WARN:"); break;
-			case 3: builder.append(" INFO:"); break;
-			case 2: builder.append("DEBUG:"); break;
-			case 1: builder.append("TRACE:"); break;
+			case 5: builder.append("\u001B[31m" + "ERROR:"); break;
+			case 4: builder.append("\u001B[33m" + " WARN:"); break;
+			case 3: builder.append("\u001B[37m" + " INFO:"); break;
+			case 2: builder.append("\u001B[34m" + "DEBUG:"); break;
+			case 1: builder.append("\u001B[36m" + "TRACE:"); break;
+			default: builder.append("     :"); break;
 		}
 		builder.append(' ');
 		builder.append('[');
-		builder.append(category);
+		builder.append(category.toUpperCase());
 		builder.append(']');
 		builder.append(' ');
 		builder.append(message);
 		if (ex != null) {
+			builder.append("\u001B[31m");
 			StringWriter writer = new StringWriter(255);
 			ex.printStackTrace(new PrintWriter(writer));
 			builder.append('\n');
 			builder.append(writer.toString().trim());
 		}
+		builder.append("\u001B[0m");
 		builder.append('\n');
 		
 		// Print string if high enough level
