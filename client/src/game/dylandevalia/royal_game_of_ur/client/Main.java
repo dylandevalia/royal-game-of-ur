a76a6a84bf2c4b701062bfa224ffb59c737ea676
Dylan Devalia <dylan@devalia.com>
2017-11-24 15:15:33 +0000

    Cleaned up code

--- client/src/game/dylandevalia/royal_game_of_ur/client/Main.java
+++ client/src/game/dylandevalia/royal_game_of_ur/client/Main.java
@@ -7,19 +7,6 @@ import javax.swing.SwingUtilities;
 public class Main {
 	
 	public static void main(String[] args) {
-//		for (int i = 0; i < 3; i++) {
-//			new Thread() {
-//				@Override
-//				public void run() {
-//					try {
-//						new ClientController().run(ServerInformation.LOCALHOST, ServerInformation.TCP_PORT);
-//					} catch (Exception e) {
-//
-//					}
-//				}
-//			}.start();
-//		}
-		
 		Log.SET_DEBUG();
 		
 		// Start running the gui in its own thread
